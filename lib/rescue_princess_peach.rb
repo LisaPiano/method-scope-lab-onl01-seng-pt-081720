28a32ecd7e2dfd7a49f86954caa4a5be590a97ab
Lisa Walters <campi_l@fortlewis.edu>
2020-08-19 05:13:07 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,5 @@
-
-def rescue_princess_peach(status)
-  if status = "rescued"
+def rescue_princess_peach
+  status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
-rescue_princess_peach("rescued")
+
